3b6e592f70b63d6f736cbae465a5640dc5d9dc4c
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-07-11 22:50:41 +0200

    fix test by creating a second expected post response
    the response is the same because in login-backend the response of login and authenticate is the same

--- tests/unit/LoginView.spec.ts
+++ tests/unit/LoginView.spec.ts
@@ -23,6 +23,10 @@ describe('LoginView.vue', () => {
   });
   test('Submit login', async () => {
     const response = { data: { id: 1, name: 'Username' } };
+
+    // response for /login
+    mockAxios.post.mockResolvedValueOnce(response);
+    // response for /authenticate
     mockAxios.post.mockResolvedValueOnce(response);
 
     const login: login = { username: 'Username', password: 'Password' };
@@ -45,7 +49,11 @@ describe('LoginView.vue', () => {
 
     await flushPromises();
 
+    // first /login gets requested 
     expect(mockAxios.post).toHaveBeenCalledWith(`${config.apiBaseUrl}/login`, login);
+
+    // after /login was requested, /authenticate gets requested to validate
+    expect(mockAxios.post).toHaveBeenCalledWith(`${config.apiBaseUrl}/authenticate`);
     expect(auth.isLoggedIn).toBe(true);
 
     expect(router.currentRoute.value.fullPath).toBe('/app');
