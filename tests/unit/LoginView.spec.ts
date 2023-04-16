0fdd1fccdbb593a08e029268a7bce9f491bf31ee
Gilian Rehm <st172032@stud.uni-stuttgart.de>
2023-04-16 15:57:46 +0200

    fix sonar issues

--- tests/unit/LoginView.spec.ts
+++ tests/unit/LoginView.spec.ts
@@ -1,8 +1,5 @@
-import { mount, VueWrapper, flushPromises } from '@vue/test-utils';
+import { VueWrapper } from '@vue/test-utils';
 import router from '@/router/index';
-import LoginView from '@/views/LoginView.vue';
-import BootstrapVue3, { BButton } from 'bootstrap-vue-3';
-import mockAxios from '@/__mocks__/axios';
 
 describe('LoginView.vue', () => {
   let wrapper: VueWrapper;
@@ -10,15 +7,10 @@ describe('LoginView.vue', () => {
   beforeEach(async () => {
     router.push('/');
     await router.isReady();
-    // wrapper = mount(LoginView, {
-    //   global: {
-    //     plugins: [BootstrapVue3, router],
-    //   },
-    // });
+
     expect(router.currentRoute.value.fullPath).toBe('/');
   });
   test('Login Button', async () => {
-    // const loginButton = wrapper.findComponent(BButton);
-    // expect(loginButton.text().includes('login'));
+
   });
 });
