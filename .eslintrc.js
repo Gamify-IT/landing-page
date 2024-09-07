f6b767bc9025233cf37827725b49cced8bf92fd1
Leonard <weasleywalker@gmx.de>
2024-09-07 14:05:08 +0200

    fixed problem with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,19 @@ module.exports = {
     node: true,
     'vue/setup-compiler-macros': true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   overrides: [
     {
